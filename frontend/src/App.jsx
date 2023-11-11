6ee0c2f454231f8e6a133fe5bd877732e424eaff
waglarry <emmanuelacquaah2468@gmail.com>
2023-11-11 05:12:57 +0000

    Update App.js with routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import HomePage from "./pages/homepage"
+import LoginPage from "./pages/login"
+import ParentDashboard from "./pages/parent-dashboard"
+import SignupPage from "./pages/signup"
+import TeacherDashboard from "./pages/teacher-dashboard"
 
 function App() {
 
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/dashboard-teacher" element={<TeacherDashboard />} />
+        <Route path="/dashboard-parent" element={<ParentDashboard />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
