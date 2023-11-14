b161d2980b988feca17c8503678d7395e4d7ebe5
waglarry <emmanuelacquaah2468@gmail.com>
2023-11-14 20:43:51 +0000

    Create Activities component

---
+++ frontend/src/pages/activities/index.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import Styles from './Activities.module.css'
+import TrustedImage from '../../assets/images/activities.svg'
+import { Link } from 'react-router-dom'
+
+const Activities = () => {
+  return (
+    <section className={Styles.section}>
+        <div className={Styles.topContainer}>
+            <div className={Styles.leftCol}>
+                <h1 className={Styles.title}>The Trusted Name for School Monitoring Activities.</h1>
+                <p className={Styles.content}>
+                    We help, you help, we all do to help our kids grow.
+                    Create an account to start with us now.
+                    We are voted as one of the best monitoring web applications.
+                </p>
+                <Link className={Styles.readMore} to={'/'}>Read more</Link>
+            </div>
+            <div className={Styles.rightCol}>
+                <img src={TrustedImage} alt="" />
+            </div>
+        </div>
+
+        <div className={Styles.topContainer}>
+            <div className={Styles.rightCol}>
+                <div className={Styles.card}>
+                    <p className={Styles.content}>
+                        Effortlessly manage your users and streamline your 
+                        workflow with our user management system.
+                    </p>
+                </div>
+            </div>
+            <div className={Styles.leftCol}>
+                <h1 className={Styles.title}>The Trusted Name for School Monitoring Activities.</h1>
+                <p className={Styles.content}>
+                    We help, you help, we all do to help our kids grow.
+                    Create an account to start with us now.
+                    We are voted as one of the best monitoring web applications.
+                </p>
+            </div>
+        </div>
+
+        <div>
+            <div className="leftCol"></div>
+            <div className="rightCol"></div>
+        </div>
+    </section>
+  )
+}
+
+export default Activities
