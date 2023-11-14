06757487228e40ff42c3f8e8e974bd7b9dff0cee
waglarry <emmanuelacquaah2468@gmail.com>
2023-11-14 20:24:24 +0000

    Update Brand wuth colors

--- frontend/src/assets/brand/index.jsx
+++ frontend/src/assets/brand/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Logo from '/momytor.svg'
+import WhiteLogo from '../images/white-brand-image.svg'
 import Styles from './Brand.module.css'
 
-const Brand = ({color}) => {
+const Brand = ({color, imgColor}) => {
   return (
     <Link to={'/'} className={Styles.brand}>
-        <img src={Logo} className={Styles.logo} alt="Brand Logo" />
+        {imgColor === 'white' ? <img src={WhiteLogo} className={Styles.logo} alt="Brand Logo" /> : <img src={Logo} className={Styles.logo} alt="Brand Logo" />}
+        
         <label className={Styles.logoText} htmlFor="logo's text" style={{color: color}}>momytor</label>
     </Link>
   )
