c8d8e8390c33c4c8f96fa6ca784510312460ec9d
waglarry <emmanuelacquaah2468@gmail.com>
2023-11-14 04:38:43 +0000

    Comment out StrictMode to prevent re-rendering

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,9 +6,9 @@ import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <MantineProvider>
       <App />
     </MantineProvider>
-  </React.StrictMode>,
+  // </React.StrictMode>,
 )
